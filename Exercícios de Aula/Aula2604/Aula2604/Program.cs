085ad11d8dfa2e1b35da580decb0d8a00fef8ca4
Francisco Vianna <francisco.v.vianna@gmail.com>
2017-04-26 15:10:55 -0300

    Inicio do exercício da aula de 26/04

---
+++ Exercícios de Aula/Aula2604/Aula2604/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2604
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string modelo;
+            int n, potencia;
+            double quilometragem;
+
+            n = int.Parse(Console.ReadLine());
+        }
+    }
+}
