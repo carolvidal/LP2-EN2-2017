55b672501d0f17b0d83f246af68b3c4d0c2c3e6e
Francisco Vianna <francisco.v.vianna@gmail.com>
2017-05-03 15:41:15 -0300

    Exercício de revisão - Etapa 4 Uso de structs para representar o carro.

--- Soluções/ExercicioRevisao/ExercicioRevisao/Program.cs
+++ Soluções/ExercicioRevisao/ExercicioRevisao/Program.cs
@@ -8,40 +8,67 @@ namespace ExercicioRevisao
 {
     class Program
     {
+        struct Carro
+        {
+            public string Modelo;
+            public double Quilometragem;
+            public int Potencia;
+        }
+
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            Carro carro;
 
             for (int i = 0; i < n; i++)
             {
-                string nome = Console.ReadLine(); //Palio
-                double quilometragem = double.Parse(Console.ReadLine()); //20000
-                int potencia = int.Parse(Console.ReadLine()); // 300
+                carro.Modelo = Console.ReadLine(); //Palio
+                carro.Quilometragem = double.Parse(Console.ReadLine()); //20000
+                carro.Potencia = int.Parse(Console.ReadLine()); // 300
 
-                Console.WriteLine(Classificar(nome, quilometragem, potencia));
+                Console.WriteLine(Classificar(carro));
                                           //"Palio"    20000          300 
             }
+
+            string s = "Francisco";
+            char c = s[5];
+            
+            char sexo = Console.ReadLine().ToUpper()[0];
+            switch(sexo)
+            {
+                case 'M': { s = "Masculino"; break; }
+                case 'F': { s = "Feminino"; break; }
+                default: throw new Exception(); 
+            }
+
+            if (sexo == 'M')
+                s = "Masculino";
+            else if (sexo == 'F')
+                s = "Feminino";
+            else
+                throw new Exception(); 
         }
 
-        public static string Classificar(string mod, double km, int pot)
-        {                               // "Palio"    20000       300
+        public static string Classificar(Carro c)
+        {
             string classifRodagem, classifPot;
 
-            if (km <= 5000)
+            if (c.Quilometragem <= 5000)
                 classifRodagem = "Novo";
-            else if (km <= 30000)
+            else if (c.Quilometragem <= 30000)
                 classifRodagem = "Seminovo";
             else
                 classifRodagem = "Velho";
 
-            if (pot < 120)
+            if (c.Potencia < 120)
                 classifPot = "Popular";
-            else if (pot <= 200)
+            else if (c.Potencia <= 200)
                 classifPot = "Forte";
             else
                 classifPot = "Potente";
 
-            return String.Format("{0} - {1} - {2}", mod, classifRodagem, classifPot);
+            return String.Format("{0} - {1} - {2}", c.Modelo, classifRodagem, classifPot);
         }
+
     }
 }
