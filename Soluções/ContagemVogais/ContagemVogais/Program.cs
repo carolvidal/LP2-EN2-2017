1bc5097adadff8746f245a2bacbfb836faf72791
Carol Werneck <werneckbsc@gmail.com>
2017-05-09 18:58:56 -0300

    Exercício de contagem de vogais - 09/05/2017

---
+++ Soluções/ContagemVogais/ContagemVogais/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContagemVogais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = Console.ReadLine();
+            int qtd = 0;
+
+            for(int i = 0; i < texto.Length; i++)
+            {
+                if(texto[i] == 'a' || texto[i] == 'A' || texto[i] == 'e' || texto[i] == 'E' || texto[i] == 'i'
+                     || texto[i] == 'I' || texto[i] == 'o' || texto[i] == 'O' || texto[i] == 'u' || texto[i] == 'U')
+                {
+                    qtd++;
+                }
+            }
+
+            Console.WriteLine("O texto fornecido contém %d vogais", qtd);
+        }
+    }
+}
