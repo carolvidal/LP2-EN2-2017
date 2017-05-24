10c31d11e51a994d73ede284bf73b54bffc79085
Francisco Vianna <francisco.v.vianna@gmail.com>
2017-05-24 16:02:39 -0300

    Transformação de Programação com subrotinas para classes

---
+++ Soluções/ExercicioRevisao/ExercicioRevisao/Carro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioRevisao
+{
+    class Carro
+    {
+        public string Modelo;
+        public double Quilometragem;
+        public int Potencia;
+        public int AnoFabricacao;
+
+        public string Classificar()
+        {
+            string classifRodagem, classifPot;
+
+            if (Quilometragem <= 5000)
+                classifRodagem = "Novo";
+            else if (Quilometragem <= 30000)
+                classifRodagem = "Seminovo";
+            else
+                classifRodagem = "Velho";
+
+            if (Potencia < 120)
+                classifPot = "Popular";
+            else if (Potencia <= 200)
+                classifPot = "Forte";
+            else
+                classifPot = "Potente";
+
+            return String.Format("{0} - {1} - {2}", Modelo, classifRodagem, classifPot);
+        }
+
+        public void AtualizarKm(double novoKm)
+        {
+            Quilometragem = novoKm;
+        }
+
+        public double TaxaDeUso()
+        {
+            return Quilometragem / (DateTime.Now.Year - AnoFabricacao);
+        }
+    }
+}
--- Soluções/ExercicioRevisao/ExercicioRevisao/Program.cs
+++ Soluções/ExercicioRevisao/ExercicioRevisao/Program.cs
@@ -8,48 +8,6 @@ namespace ExercicioRevisao
 {
     class Program
     {
-        public struct Carro
-        {
-            public string Modelo;
-            public double Quilometragem;
-            public int Potencia;
-            public int AnoFabricacao;
-        }
-
-        public static string Classificar(ref Carro c)
-        {
-            string classifRodagem, classifPot;
-
-            if (c.Quilometragem <= 5000)
-                classifRodagem = "Novo";
-            else if (c.Quilometragem <= 30000)
-                classifRodagem = "Seminovo";
-            else
-                classifRodagem = "Velho";
-
-            if (c.Potencia < 120)
-                classifPot = "Popular";
-            else if (c.Potencia <= 200)
-                classifPot = "Forte";
-            else
-                classifPot = "Potente";
-
-            return String.Format("{0} - {1} - {2}", c.Modelo, classifRodagem, classifPot);
-        }
-
-        public static void AtualizarKmCarro(ref Carro c, double novoKm)
-        {
-            c.Quilometragem = novoKm;
-        }
-
-        public static double TaxaDeUsoCarro(ref Carro c)
-        {
-            return c.Quilometragem / (DateTime.Now.Year - c.AnoFabricacao);
-        }
-
-
-
-
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
@@ -57,13 +15,14 @@ namespace ExercicioRevisao
 
             for (int i = 0; i < n; i++)
             {
+                carros[i] = new Carro();
                 carros[i].Modelo = Console.ReadLine();
                 carros[i].Quilometragem = double.Parse(Console.ReadLine());
                 carros[i].Potencia = int.Parse(Console.ReadLine());
             }
 
             for (int i = 0; i < n; i++)
-                Console.WriteLine(Classificar(ref carros[i]));
+                Console.WriteLine(carros[i].Classificar());
         }
     }
 }
